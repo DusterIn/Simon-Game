2a50e8c9ecd76926afe7c5f72d45220bd0490d80
Akhil Naidu <54505339+DusterIn@users.noreply.github.com>
2020-10-06 18:54:33 +0530

    Update game.js

--- game.js
+++ game.js
@@ -27,7 +27,6 @@ $(".btn").click(function() {
 });
 
 function checkAnswer(currentLevel) {
-
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
